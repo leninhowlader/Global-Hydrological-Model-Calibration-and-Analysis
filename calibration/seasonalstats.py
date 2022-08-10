f710b231c07709da44f0c23e4b9b8bbb202b21b0
mhasan <mhasan@gfz-potsdam.de>
2022-08-10 12:18:11 +0200

    functions added for (1) identifying seasons and (2) computing seasonal amplitudes

--- calibration/seasonalstats.py
+++ calibration/seasonalstats.py
@@ -169,3 +169,105 @@ class SeasonalStatistics(stats):
         except: succeed = False
 
         return succeed
+
+    @staticmethod
+    def get_seasonal_amplitudes(
+        data:np.ndarray,
+        season:np.ndarray=np.empty(0),
+    ):
+        """
+        Finds seasonal amplitudes for each season.
+
+        Parameters:
+        @param data: (2-d numpy array) data. each column must represent a time-
+                        series
+        @param season: (1-d numpy array, optional) hydrological season. this
+                        should have 12 integer values codifying to which season
+                        the monthly values, starting from Jan to Dec, belongs to.
+                        however, defining season for other than monthly time-
+                        step is also possible.
+                        the lowest value in the season variable would indicate 
+                        the start of the first season. if a hydrological season
+                        occurs in month belonging to two years, appropriate 
+                        shifting will be done
+        @return (1-d or 2-d numpy array) the list of amplitudes for each season 
+                        in the order of occurance of the hydrological season
+        """
+
+        ## [step] create season identifier array
+        season_array = SeasonalStatistics.season_identifier_array(
+            n=data.shape[0],
+            season=season
+        )
+        ## [end]
+
+        ## [step] delete data for fractional seasons at the beginning and at
+        ## the end
+        ii = np.isnan(season_array)
+        data = data[~ii]
+        season_array = season_array[~ii]
+        ## [end]
+
+        amplitudes = np.empty(0)
+        for sn in np.unique(season_array):
+            ii = (season_array==sn)
+            d = data[ii]
+            a = (np.nanmax(d, axis=0) - np.nanmin(d, axis=0)).reshape(1, -1)
+            
+            try: amplitudes = np.concatenate((amplitudes, a), axis=0)
+            except: amplitudes = a
+        
+        return amplitudes
+
+
+    @staticmethod
+    def season_identifier_array(
+        n:int,
+        season:np.ndarray=np.empty(0),
+    ):
+        """
+        The function generates an array containing unique identifiers for 
+        successive seasons until the end of time step reach
+
+        Paramters:
+        @paran n: (int) number of time steps in total
+        @param season: (1-d numpy array) describes the seasons in each successive
+                        time steps till the season repeats. the lowest value in 
+                        the season variable would indicate 
+                        the start of the first season. if a hydrological season
+                        occurs in month belonging to two years, appropriate 
+                        shifting will be done
+        @return (1-d numpy array of int) season identifier array
+        """
+        season = np.array(season)
+        if season.shape[0] == 0: season = np.zeros(12, dtype=int)
+        
+        ## [step] shift seasons, if necessary
+        first_sni = np.argsort(season)[0]
+        if first_sni > 0:
+            season = np.concatenate((season[first_sni:],season[:first_sni]), 
+                                    axis=0)
+        ## [end]
+
+        ## [step] create season identifier array
+        nseasons = np.unique(season).shape[0]
+        lp = 0                  # no. of loops
+        season_all = np.array([np.nan] * first_sni)
+        while season_all.shape[0] < n:
+            season_all = np.concatenate((season_all, season + (lp * nseasons)), 
+                                        axis=0)
+            lp += 1
+        season_all = season_all[:n]
+        ## [end]
+
+        ## [step] discard values from the end, if necessary
+        last_sn = np.nanmax(season_all)
+        sn_actual = last_sn - (lp - 1) * nseasons
+
+        count0 = (season==sn_actual).sum()
+        count1 = (season_all==last_sn).sum()
+        if count0 != count1: season_all[season_all==last_sn] = np.nan
+        ## [end]
+
+        return season_all
+
